ec5cff5dac7308d5dd557487752816bd1701e894
Katrina Chan <katrina.hyc@gmail.com>
2016-08-30 16:58:09 +0800

    Modify generator; add GM slide

--- gen/app.js
+++ gen/app.js
@@ -2,12 +2,13 @@ var fs = require("fs");
 
 var file_name = process.argv[2];
 var title = process.argv[3];
-var output_file_name = process.argv[4];
 
-var template = fs.readFileSync("./template.html", "utf-8");
-var content  = fs.readFileSync("../src/" + file_name, "utf-8");
+var template   = fs.readFileSync("./template.html", "utf-8");
+var content    = fs.readFileSync("../src/" + file_name + ".md", "utf-8");
+var stylesheet = fs.readFileSync("./style.css", "utf-8");
 
-var output = template.replace("{{title}}",   title)
-                     .replace("{{content}}", content);
+var output = template.replace("{{title}}",      title)
+                     .replace("{{stylesheet}}", stylesheet)
+                     .replace("{{content}}",    content);
 
-fs.writeFileSync("../" + output_file_name, output, "utf-8");
+fs.writeFileSync("../" + file_name + ".html", output, "utf-8");
