fd0b3e48f12cb7b39746d199d60e56b533eb1537
Katrina Chan <katrina.hyc@gmail.com>
2016-09-13 16:37:32 +0800

    Add npm run-script

--- gen/app.js
+++ gen/app.js
@@ -3,12 +3,12 @@ var fs = require("fs");
 var file_name = process.argv[2];
 var title = process.argv[3];
 
-var template   = fs.readFileSync("./template.html", "utf-8");
-var content    = fs.readFileSync("../src/" + file_name + ".md", "utf-8");
-var stylesheet = fs.readFileSync("./style.css", "utf-8");
+var template   = fs.readFileSync("gen/template.html", "utf-8");
+var content    = fs.readFileSync("src/" + file_name + ".md", "utf-8");
+var stylesheet = fs.readFileSync("gen/style.css", "utf-8");
 
 var output = template.replace("{{title}}",      title)
                      .replace("{{stylesheet}}", stylesheet)
                      .replace("{{content}}",    content);
 
-fs.writeFileSync("../" + file_name + ".html", output, "utf-8");
+fs.writeFileSync("./" + file_name + ".html", output, "utf-8");
